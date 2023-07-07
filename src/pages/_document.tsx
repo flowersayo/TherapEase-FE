68bb7d8aee34f60c79b924c2ac3bcddb8dddd245
권가은 <aileen1007@ewhain.net>
2023-07-07 20:34:03 +0900

    feat: 디자인 시스템 변경사항 반영

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,10 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
     <Html lang="en">
       <Head />
+      <title>TherapEase - Make Your Counseling Perfect</title>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 // 랜딩 페이지
 const Home = () => {
-  return <div className="text-Red">랜딩 페이지 입니다.</div>;
+  return <div className="text-red-100">랜딩 페이지 입니다.</div>;
 };
 
 export default Home;
