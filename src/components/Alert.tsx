177382d070a8cc180380a427eee8a9ca7d3bf132
Gaeun Kwon <65700066+Gaeun-Kwon@users.noreply.github.com>
2023-07-24 16:10:46 +0900

    fix: Alert position 수정 (#36)

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -5,15 +5,12 @@ interface Props {
 }
 
 const Alert = ({ text }: Props) => {
-  // const textHalfLength = 8.4 + 1.4 * (text.length / 2);
-  // left-[calc(50vw-8.4rem)]
-
   return (
-    <div className="absolute top-[calc(100vh-13rem)] left-[calc(50vw-22rem)] flex">
+    <div className="fixed bottom-[13rem] left-2/4 translate-x-[-50%] flex">
       <div
-        className={` text-gray-3 text-body3 bg-[#0000004d] px-[8.4rem] py-[0.6rem] rounded-[3rem]`}
+        className={`text-gray-3 text-body3 bg-[#0000004d] px-[8.4rem] py-[0.6rem] rounded-[3rem]`}
       >
-        {text}
+        권가은{text}
       </div>
     </div>
   );
